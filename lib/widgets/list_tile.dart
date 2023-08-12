233336c4d2345682d3552d1a30f84f479a6048ca
oalshokri <janataanfor@gmail.com>
2023-08-12 21:35:39 +0300

    local and global state

--- lib/widgets/list_tile.dart
+++ lib/widgets/list_tile.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Buy Milk'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        'Buy Milk',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: MyCheckbox(
+        isChecked: isChecked,
+        toggleCheckboxState: (value) {
+          setState(() {
+            isChecked = value!;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class MyCheckbox extends StatelessWidget {
+  final bool isChecked;
+  final Function(bool?)? toggleCheckboxState;
+  const MyCheckbox({
+    super.key,
+    required this.isChecked,
+    required this.toggleCheckboxState,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: toggleCheckboxState,
     );
   }
 }
